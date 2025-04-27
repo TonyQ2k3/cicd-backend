4bbbfa7548f798431a814092afc4922f8b0f6594
TonyQ2k3 <ntbquoc2015@gmail.com>
2025-04-27 15:39:51 +0700

    Added post handler

--- src/app.js
+++ src/app.js
@@ -9,21 +9,42 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+const {
+    getPosts,
+    getOnePost,
+    createPost,
+    deletePost
+} = require('./controllers/postController')
+
+const authRoutes = require('./controllers/authController');
+
+
 app.get('/', (req, res) => {
-    res.send("Authentication Service is running!");
+    res.send("Backend is running!");
 });
+
+// Post routes
+
+app.get('/posts/status', (req, res) => {
+    res.send("Post Service is running!");
+})
+app.get('/posts/all', getPosts);
+app.get('/posts/all/:id', getOnePost);
+app.post('/posts', createPost);
+app.delete('/posts/:id', deletePost);
+
+
+// Auth routes
 
 app.get('/set-cookie', (req, res) => {
     res.cookie('current_user', true);
     res.send("Cookie for user set");
 });
-
 app.get('/get-cookie', (req, res) => {
     const cookies = req.cookies;
     res.json(cookies);
 })
 
-const authRoutes = require('./routes/authRoutes');
 app.use(authRoutes);
 
 
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
    .then(() => {
         app.listen(port, () => {
             console.log('Connected to database');
-            console.log(`Auth service listening at http://localhost:${port}`);
+            console.log(`Back-end listening at http://localhost:${port}`);
         });
    })
    .catch(err => console.log(err));
